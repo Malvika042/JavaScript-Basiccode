32f819047f2158da09b73cbe71e0dd6dedc5406e
Malvika <101415369+Malvika042@users.noreply.github.com>
2024-06-07 15:34:32 +0530

    part-1

--- basic.js
+++ basic.js
@@ -1,8 +1,29 @@
-const accountId = 1234
+// const accountId = 1234
+// let accountEmail = "malvika@gmail.com"
+// var accountpassword = "45667"
+// city = "delhi"
+
+// //accountId cannot change
+
+// console.log(accountId);
+
+// Basic of JavaScript (part-1)
+const accountId = 1234 // constant values cannot be changed
 let accountEmail = "malvika@gmail.com"
-var accountpassword = "45667"
-city = "delhi"
+var password = "123456"           // 
+accountCity = "delhi" //can be understood by the compiler about the let/var/const
 
-//accountId cannot change
+//accountId = 7777  const value cannot be altered
+console.log(accountEmail) 
 
-console.log(accountId);
+accountEmail = "malvikaaaa@gmail.accountEmail"
+password = "5668765"
+
+console.log(password) // giving me the updated value of the password
+//console.log("password")  print the value inside the double inverted commas
+console.table([accountEmail,accountCity,accountId,password])
+/* initial days :- no scope taken under the consideration
+ prefer not to use Var because of the issue in block and functional scope
+*/
+
+
