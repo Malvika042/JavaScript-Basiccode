4e988191dbf100379de0a84be1b07fa847b7fc1e
Malvika <101415369+Malvika042@users.noreply.github.com>
2024-06-07 15:36:03 +0530

    basic-2 js

---
+++ 2js.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+// code should be readable, clean, and easy to understand 
+
+
+
+
+
+//datatypes
+//number
+//bigint
+//string
+//boolean => true, false
+// null =>standalone value represenation of empty value
+//undefined =? when you havenot declare any value
+//symbol => unique 
+
+
+
+//object
+console.log(typeof undefined); // undefined
+console.log(typeof "null"); // object
---
+++ 3js.js
@@ -0,0 +1,27 @@
+// let score = "33"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueinNumber = Number (score)
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+// "33" => number 
+
+// let score = "33abc"   // this string is not converted into the number and giving the value NAN
+// // NOT A NUMBER
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueinNumber = Number (score)
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+
+
+let score = 0   // this string is not converted into the number and giving the value NAN
+// NOT A NUMBER
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinNumber = Boolean (score)
+console.log(typeof valueinNumber);
+console.log(valueinNumber);   // giving true
